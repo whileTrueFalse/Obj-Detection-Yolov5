d40190c16ec3ade4b912fad39446c9d4760faa22
whileTrueFalse <117916649+whileTrueFalse@users.noreply.github.com>
2024-11-12 22:33:11 +0530

    oo

--- yolov5-streamlit-main/main.py
+++ yolov5-streamlit-main/main.py
@@ -84,8 +84,8 @@ if __name__ == '__main__':
             with st.spinner(text='Resources loading...'):
                 st.sidebar.image(uploaded_file)
                 picture = Image.open(uploaded_file)
-                picture.save(f'yolov5-streamlit-main/data/images{uploaded_file.name}')
-                opt.source = f'yolov5-streamlit-main/data/images{uploaded_file.name}'
+                picture.save(f'data/images{uploaded_file.name}')
+                opt.source = f'data/images{uploaded_file.name}'
         else:
             is_valid = False
     else:
@@ -96,7 +96,7 @@ if __name__ == '__main__':
                 st.sidebar.video(uploaded_file)
                 with open(os.path.join("yolov5-streamlit-main", "data", "videos", uploaded_file.name), "wb") as f:
                     f.write(uploaded_file.getbuffer())
-                opt.source = f'yolov5-streamlit-main/data/videos{uploaded_file.name}'
+                opt.source = f'data/videos{uploaded_file.name}'
         else:
             is_valid = False
 
